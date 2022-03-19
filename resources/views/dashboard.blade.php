9cd6e1ac861461dbb9d17827052b9d69bc5b1d57
pimnipaaa <pimnipa.sr@kkumail.com>
2022-03-20 01:40:16 +0700

    changhome

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
-    <!--<p>jjiiiiiiiiiiiii</p>-->
-    <button class="btn btn-primary">kkkkk</button>
-</x-app-layout>
+<div class="banner"></div>
+    <div class="content">Beok
+        <p> เว็บไซต์ขายหนังสือที่จะทำให้คุณเพลิดเพลินไปกับการเลือกซื้อหนังสือและได้รับรู้เกี่ยวกับรายละเอียดหนังสือที่คุณต้องการจะหามัน  
+            คุณสามารถที่จะอ่านเรื่องย่อของหนังสือที่ต้องการและมีโปรโมชั่นมากมายรอคุณอยู่ </p>
+        <a href="log_reg/login.php"><button type="button" style="font-family: Prompt; font-size: large;" class ="btn-page">เข้าสู่เว็บไซต์</button></a>  
+    </div>
+
+</x-app-layout> 
